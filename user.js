897db4b9aa9ae23838cdbc1bae6630b2d06ee8c4
kwonje8761 <kje_9912@naver.com>
2021-03-17 18:05:48 +0900

    commit 0317

---
+++ user.js
@@ -0,0 +1,18 @@
+document.getElementById('goto_face').addEventListener("click", async (event) => {
+
+   var contents = `
+        <table>
+            <tr>
+                <td style="width:350px; border-right: 1px solid black;">Date</td>
+                <td style="width:350px; border-right: 1px solid black;">Time</td>
+                <td style="width:400px; ">Location</td>
+            </tr>
+
+            <tr>
+                <td 
+
+            </tr>
+        </table>
+   `
+    document.getElementById('user_content').innerHTML = contents;
+})
